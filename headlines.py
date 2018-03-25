147f985cbb30033c7c5051f18472968a794821d7
BryanJDaniel <bryan.j.daniel@gmail.com>
2018-03-25 09:54:45 -0700

    Changed urllib.request to urllib2

--- headlines.py
+++ headlines.py
@@ -5,10 +5,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
-import urllib.request
-import urllib.parse
+# import urllib.request
+# import urllib.parse
+import urllib2
 import urllib
-
 
 app = Flask(__name__)
 
@@ -18,71 +18,71 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 'fox': 'http://feeds.foxnews.com/foxnews/latest',
 'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
-#metric-units
+#METRIC UNITS
 #WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=8218c490e1db3cf7355a8fe1d6b41a4b"
-#freedom units
+#IMPERIAL (US) UNITS
 WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&APPID=8218c490e1db3cf7355a8fe1d6b41a4b"
 CURRENCY_URL = "https://openexchangerates.org//api/latest.json?app_id=cefbae52dfae4e709f65288bcb711dee"
 
-DEFAULTS = {'publication':'bbc',
-			'city': 'London,UK',
-			'currency_from': 'GBP',
-			'currency_to': 'USD'
-			}
+DEFAULTS = {'publication': 'bbc',
+            'city': 'London,UK',
+            'currency_from': 'GBP',
+            'currency_to': 'USD'
+            }
+
 
 @app.route("/")
 def home():
-	# get customized headlines, based on user input or default
-	publication = request.args.get('publication')
-	if not publication:
-		publication = DEFAULTS['publication']
-	articles = get_news(publication)
-	# get customized weather based on user input or default
-	city = request.args.get('city')
-	if not city:
-		city = DEFAULTS['city']
-	weather = get_weather(city)
-	# get customized currency based on user input or default
-	currency_from = request.args.get("currency_from")
-	if not currency_from:
-		currency_from = DEFAULTS['currency_from']
-	currency_to = request.args.get("currency_to")
-	if not currency_to:
-		currency_to = DEFAULTS['currency_to']
-	rate, currencies = get_rate(currency_from, currency_to)
-	return render_template("home.html", articles=articles, weather=weather,
+    # get customised headlines, based on user input or default
+    publication = request.args.get('publication')
+    if not publication:
+        publication = DEFAULTS['publication']
+    articles = get_news(publication)
+    # get customised weather based on user input or default
+    city = request.args.get('city')
+    if not city:
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    # get customised currency based on user input or default
+    currency_from = request.args.get("currency_from")
+    if not currency_from:
+        currency_from = DEFAULTS['currency_from']
+    currency_to = request.args.get("currency_to")
+    if not currency_to:
+        currency_to = DEFAULTS['currency_to']
+    rate, currencies = get_rate(currency_from, currency_to)
+    return render_template("home.html", articles=articles, weather=weather,
                            currency_from=currency_from, currency_to=currency_to, rate=rate,
                            currencies=sorted(currencies))
 
+
 def get_rate(frm, to):
-	all_currency = urllib.request.urlopen(CURRENCY_URL).read()
+    all_currency = urllib2.urlopen(CURRENCY_URL).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return (to_rate / frm_rate, parsed.keys())
 
-	parsed = json.loads(all_currency).get('rates')
-	frm_rate = parsed.get(frm.upper())
-	to_rate = parsed.get(to.upper())
-	return(to_rate/frm_rate , parsed.keys())
 
-def get_news(query):
-	if not query or query.lower() not in RSS_FEEDS:
-		publication = DEFAULTS["publication"]
-	else:
-		publication = query.lower()
-	feed = feedparser.parse(RSS_FEEDS[publication])
-	return feed['entries']
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    return feed['entries']
+
 
 def get_weather(query):
-	query = urllib.parse.quote(query)
-	url = WEATHER_URL.format(query)
-	data = urllib.request.urlopen(url).read()
-	parsed = json.loads(data)
-	weather = None
-	if parsed.get('weather'):
-		weather = {'description':parsed['weather'][0]['description'],
-		'temperature':parsed['main']['temp'],
-		 'city':parsed['name'],
-		 'country': parsed['sys']['country']
-		}
-	return weather
+    query = urllib.quote(query)
+    url = WEATHER_URL.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description': parsed['weather'][0]['description'],
+                   'temperature': parsed['main']['temp'],
+                   'city': parsed['name'],
+                   'country': parsed['sys']['country']
+                   }
+    return weather
 
-if __name__ == '__main__':
-	app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+
